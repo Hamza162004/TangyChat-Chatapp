14e8eafb617527ee3fe8f783820e092bfde83d08
muzamil-14 <158832335+muzamil-14@users.noreply.github.com>
2024-10-03 01:31:52 +0500

    Added interceptor and ensured proper working of signUp Api, also adjust few things accordingly

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -5,79 +5,81 @@ import { ErrorHandler } from "../utils/utility.js";
 import { uploadToCloudinary } from "../utils/cloudinary.js";
 
 const signup = async (req, res) => {
-    try {
-        if(!req.file){
-            res.status(400).json({message : 'Plz Send an avatar'})
-            return
-        }
-        const result = await uploadToCloudinary(req.file.buffer , 'Tangy-avatar')
-        
-        const { name, username, bio, password } = req.body
-        const avatar = {
-            public_id: result.public_id,
-            url: result.url,
-        }
-        const user = await User.create({
-            name,
-            username,
-            password,
-            avatar,
-            bio,
-        })
-
-        sendToken(res, user, 201, `User ${username} created `)
-    } catch (error) {
-       res.json({error})
+  try {
+    if (!req.file) {
+      res.status(400).json({ message: "Plz Send an avatar" });
+      return;
     }
+    console.log(req.file)
+    console.log("Body------->" , req.body)
+    const result = await uploadToCloudinary(req.file.buffer, "Tangy-avatar");
 
-}
+    const { username, bio, email, password } = req.body;
+
+    const avatar = {
+      public_id: result.public_id,
+      url: result.url,
+    };
+
+    const user = await User.create({
+      username,
+      bio,
+      email,
+      password,
+      avatar
+    });
+
+    sendToken(res, user, 201, `User ${username} created `);
+  } catch (error) {
+    res.json({ error });
+  }
+};
 
 const login = async (req, res, next) => {
-    try {
-        const { username, password } = req.body
-        const user = await User.findOne({ username }).select('+password')
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username }).select("+password");
 
-        if (!user) {
-            return next(new ErrorHandler('Invalid Username', 404))
-        }
-        const isMatch = await compare(password, user.password)
-        if (!isMatch) {
-            return next(new ErrorHandler('Invalid Password', 404))
-        }
-        sendToken(res, user, 200, "Welcome back!")
-    } catch (error) {
-        next(error)
+    if (!user) {
+      return next(new ErrorHandler("Invalid Username", 404));
     }
+    const isMatch = await compare(password, user.password);
+    if (!isMatch) {
+      return next(new ErrorHandler("Invalid Password", 404));
+    }
+    sendToken(res, user, 200, "Welcome back!");
+  } catch (error) {
+    next(error);
+  }
+};
 
-}
-
-const logout = async (req,res,next)=>{
-    res.status(200).cookie("Tangy-token","",{...cookieOption,maxAge:0})
+const logout = async (req, res, next) => {
+  res
+    .status(200)
+    .cookie("Tangy-token", "", { ...cookieOption, maxAge: 0 })
     .json({
-        success:true,
-        message:'logged out'
-    })
-}
+      success: true,
+      message: "logged out",
+    });
+};
 
 const getMyProfile = async (req, res, next) => {
-    try {
-        const user = await User.findById(req.user)
+  try {
+    const user = await User.findById(req.user);
 
-        res.json({ sucess: true, user })
-    } catch (error) {
-        next(error)
-    }
+    res.json({ sucess: true, user });
+  } catch (error) {
+    next(error);
+  }
+};
 
-}
+const searchUser = async (req, res, next) => {
+  const { name } = req.query;
 
-const searchUser = async (req,res,next)=>{
-    const {name} = req.query
-
-    res.status(200)
-    .json({
-        success:true,
-        message:name
-    })
-}
+  res.status(200).json({
+    success: true,
+    message: name,
+  });
+};
 
 export { login, signup, getMyProfile, logout, searchUser };
--- server/models/user.js
+++ server/models/user.js
@@ -3,15 +3,16 @@ import mongoose , { Schema, model} from 'mongoose';
 
 
 const schema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
     username: {
         type: String,
         required: true,
         unique: true
     },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+      },
     password: {
         type: String,
         required: true,
