f3deba5eb20f8b3a317505fb7ae40f6cd9599ad6
Hamza162004 <143029327+Hamza162004@users.noreply.github.com>
2024-10-06 20:19:11 +0500

    final

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,17 +1,18 @@
 import { ErrorHandler } from "../utils/utility.js";
 import jwt from 'jsonwebtoken'
 
-
-const isLoggedIn =  (req, res, next) => {
-    const token = req.cookies['Tangy-token'];
-    if (!token) {
-        return next(new ErrorHandler("Please login", 401));
+const isLoggedIn = (req, res, next) => {
+    try {
+        const token = req.header('Tangy-token');
+        if (!token) {
+            return next(new ErrorHandler("Please login", 401));
+        }
+        const data = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = data._id;
+        next();
+    } catch (error) {
+        console.log("Fetch token error")
     }
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded._id
-    console.log({User:req.user})
-    next()
 }
 
 export { isLoggedIn }
