35f2fe4e9c7ef6956d98b36d4578cd67234cec11
Hamza162004 <143029327+Hamza162004@users.noreply.github.com>
2024-10-06 18:45:39 +0500

    A few apis for chats

--- server/controllers/chat.js
+++ server/controllers/chat.js
@@ -1,5 +1,7 @@
 import { Chat } from "../models/chat.js";
+import { Message } from "../models/message.js";
 import { User } from "../models/user.js";
+import { emitEvent } from "../utils/features.js";
 import { getOtherMember } from "../utils/helper.js";
 import { ErrorHandler } from "../utils/utility.js";
 
@@ -77,6 +79,9 @@ const getGroupChats = async (req, res, next) => {
 const addGroupMember = async(req,res,next)=>{
     const {chatId , members} = req.body
     const chat = await Chat.findById(chatId)
+    if(!members || members.length<1){
+        return next(new ErrorHandler(`No members provided`, 400))
+    }
     if (!chat) {
         return next(new ErrorHandler(`No Chat with Id ${chatId} exists`, 400))
     }
@@ -99,11 +104,174 @@ const addGroupMember = async(req,res,next)=>{
         return next(new ErrorHandler(`All members are already in the group`, 400));
     }
     
-    chat.members.push(...newMembers.map((i)=>i._id))
+    chat.members.push(...membersToAdd.map((i)=>i._id))
+    await chat.save()
+    const allMembersName = membersToAdd.map((i)=>i.username).join(',')
+
+    emitEvent(req,'ALERT',chat.members,`${allMembersName} have been added to the group`)
+    emitEvent(req,'REFRESH_CHATS',chat.members)
+
+    return res.status(200).json({ success: true, message : "Members added to the group" });
+}
+
+const removeGroupMember = async(req,res,next)=>{
+    const {chatId , userId} = req.body
+    const [chat,userToRemove]= await Promise.all([
+        Chat.findById(chatId),
+        User.findById(userId)
+    ])
+    
+    if (!chat) {
+        return next(new ErrorHandler(`No Chat with Id ${chatId} exists`, 400))
+    }
+    if(!userToRemove){
+        return next(new ErrorHandler(`No user Fonud`, 400))
+    }
+    if (!chat.groupChat) {
+        return next(new ErrorHandler(`Not a group chat`, 400))
+    }
+    if (chat.creator.toString()!==req.user.toString()) {
+        return next(new ErrorHandler(`Only admin can add members`, 403))
+    }
+    if (chat.members.includes(userToRemove._id.toString())) {
+        return next(new ErrorHandler(`User does not belong to the group`, 403))
+    } 
+    if(chat.members.length <= 3){
+        return next(new ErrorHandler(`Group should have at least 3 members`, 403))
+    }
+
+    
+    chat.members = chat.members.filter((i)=> i._id!== userId)
     await chat.save()
 
-    return res.status(200).json({ success: true, group: chat });
+    emitEvent(req,'ALERT',chat.members,`${userToRemove.username} has been removed from the group`)
+    emitEvent(req,'REFRESH_CHATS',chat.members)
+
+    return res.status(200).json({ success: true, message: "Member removed from the group" });
+}
+
+const leaveGroup = async(req,res,next)=>{
+    const {id} = req.params
+    const [chat,user]= await Promise.all([
+        Chat.findById(id),
+        User.findById(req.user)
+    ])
+     
+    if (!chat) {
+        return next(new ErrorHandler(`No Chat with Id ${chatId} exists`, 400))
+    }
+    if (!chat.groupChat) {
+        return next(new ErrorHandler(`Not a group chat`, 400))
+    }
+    if (!chat.members.includes(user._id.toString())) {
+        return next(new ErrorHandler(`User does not belong to the group`, 403))
+    }
+    if(chat.members.length <= 3){
+        return next(new ErrorHandler(`Group should have at least 3 members`, 403))
+    }
+    
+
+    const remaimingMem = chat.members.filter((i)=> i._id.toString() !== user._id.toString())
+    console.log({remaimingMem})
+
+    if (chat.creator.toString()===user._id.toString()) {
+        chat.creator = remaimingMem[0]
+    }
+
+    chat.members = remaimingMem
+    await chat.save()
+
+    emitEvent(req,'ALERT',chat.members,`${user.username} has left the group`)
+
+    return res.status(200).json({ success: true, message: "You have left the group" });
+}
+
+const sendAttachments = async(req,res,next)=>{
+    const {chatId} = req.body
+    const [chat,user]= await Promise.all([
+        Chat.findById(chatId),
+        User.findById(req.user)
+    ])
+     
+    if (!chat) {
+        return next(new ErrorHandler(`No Chat with Id ${chatId} exists`, 400))
+    }
+    if (!chat.members.includes(user._id.toString())) {
+        return next(new ErrorHandler(`User does not belong to the Chat`, 403))
+    }
+    
+    const files = req.files || []
+
+    if(files.length < 1){
+        return next(new ErrorHandler('No files attached',400))
+    }
+
+    const attachments = []
+
+    const messageRealTime = {chat:chatId , sender : { name:user.username , avatar : user.avatar , _id : user._id} , content:"" , attachments}
+    const messageDB = {chat:chatId , sender : user._id , content:"" , attachments}
+    const message = await Message.create(messageDB)
+    emitEvent(req,'NEW_ATTACHMENTS',chat.members,{message:messageRealTime , chatId})
+    emitEvent(req,'NEW_MESSAGE',chat.members,{chatId})
+
+    return res.status(200).json({ success: true, message });
+}
+
+const getChatDetails = async(req,res,next)=>{
+    const {id} = req.params
+    if(req.query.populate){
+        const chat = await Chat.findById(id).populate("members","username avatar").lean()
+        chat.members = chat.members.map((i)=> {return{avatar : i.avatar.url , name : i.username}})
+        return res.status(200).json({ success: true, chat });
+
+    }else{
+        const chat =await Chat.findById(id)
+        return res.status(200).json({ success: true, chat });
+    }
+}
+
+const renameChat = async(req,res,next)=>{
+    const {id} = req.params
+    const {name} = req.body
+    
+    const chat =await Chat.findById(id)
+    if (!chat) {
+        return next(new ErrorHandler(`No Chat with Id ${id} exists`, 400))
+    }
+    if (!chat.groupChat) {
+        return next(new ErrorHandler(`Not a group chat`, 400))
+    }
+    if (chat.creator.toString()!==req.user.toString()) {
+        return next(new ErrorHandler(`Only admin can rename`, 403))
+    }
+    chat.name = name
+    await chat.save()
+    emitEvent(req,'REFRESH_CHATS',chat.members)
+
+    return res.status(200).json({ success: true, chat });
+    
+}
+
+const deleteChat = async(req,res,next)=>{
+    const {id} = req.params
+    
+    const chat =await Chat.findById(id)
+    if (!chat) {
+        return next(new ErrorHandler(`No Chat with Id ${id} exists`, 400))
+    }
+    if (chat.groupChat && chat.creator.toString()!==req.user.toString()) {
+        return next(new ErrorHandler(`You are not allowed to delete the group`, 400))
+    }
+    if (!chat.groupChat && !chat.members.includes(req.user.toString())) {
+        return next(new ErrorHandler(`You are not allowed to delete the chat`, 403))
+    }
+    emitEvent(req,'REFRESH_CHATS',chat.members)
+
+    return res.status(200).json({ success: true});   
 }
 
 
-export { createGroup, getMyChats , getGroupChats, addGroupMember }
+
+
+
+export { createGroup, getMyChats , getGroupChats, addGroupMember , removeGroupMember,renameChat , leaveGroup , sendAttachments , getChatDetails, deleteChat }
