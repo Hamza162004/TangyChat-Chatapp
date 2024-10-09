c27b9242f2064f32d19fa395ae2134348ebbe037
muzamil-14 <158832335+muzamil-14@users.noreply.github.com>
2024-10-10 01:50:20 +0500

    Completed Intergration for User Profile, also used redux to limit API hitting in fetching notification and profile info

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import { HelmetProvider } from 'react-helmet-async'
 import { CssBaseline } from '@mui/material'
 import { Toaster } from 'react-hot-toast'
 import { Provider } from 'react-redux'
-import store from './store/store.js'
+import store from './redux/store.js'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
