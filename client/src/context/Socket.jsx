b529e028f6fef0c2ec039614f30c007af28efc1a
Hamza162004 <143029327+Hamza162004@users.noreply.github.com>
2024-11-02 22:00:06 +0500

    fixed socket error on token expiry

--- client/src/context/Socket.jsx
+++ client/src/context/Socket.jsx
@@ -1,20 +1,29 @@
 import { createContext, useContext, useEffect, useMemo } from 'react'
-import {io} from 'socket.io-client'
+import { io } from 'socket.io-client'
 import storageService from '../service/storageService'
+import { useNavigate } from 'react-router-dom';
 
 const SocketContext = createContext()
 const API_URL = import.meta.env.VITE_API_URL;
 
-const getSocket = ()=> useContext(SocketContext)
+const getSocket = () => useContext(SocketContext)
 
 const SocketProvider = ({ children }) => {
-    const socket = useMemo(()=> 
-      io(API_URL,{
-        extraHeaders:{
-            'Tangy-token' : storageService.getToken()
-        }
+  const navigate = useNavigate()
+  const socket = useMemo(() =>{
+    const newSocket = io(API_URL, {
+      extraHeaders: {
+        'Tangy-token': storageService.getToken()
+      }
     })
-  ,[storageService.getToken()])
+    newSocket.on('connect_error',(err)=>{
+      if(err.message==='jwt expired'){
+       storageService.removeToken();
+        navigate("/");
+      }
+    })
+    return newSocket
+  }, [storageService.getToken()])
 
   //   useEffect(() => {
   //     return () => {
@@ -22,11 +31,11 @@ const SocketProvider = ({ children }) => {
   //     };
   // }, [socket]);
 
-    return (
-      <SocketContext.Provider value={socket}>
-        {children}
-      </SocketContext.Provider>
-    );
+  return (
+    <SocketContext.Provider value={socket}>
+      {children}
+    </SocketContext.Provider>
+  );
 };
 
-export {SocketProvider , getSocket}
+export { SocketProvider, getSocket }
