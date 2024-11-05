ae7ef592ab87789837c181fe3cb10c4d0b9da556
Hamza Javed <143029327+Hamza162004@users.noreply.github.com>
2024-11-05 18:53:17 +0500

    Update socket.js

--- server/socket.js
+++ server/socket.js
@@ -14,12 +14,7 @@ const getSockets = (users) => {
 }
 
 const initializeSocket = (server) => {
-    const io = new Server(server, {cors:{    
-    origin: "https://tangy-chat-frontend.vercel.app",
-    allowedHeaders: ["tangy-token"],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
-    });
+    const io = new Server(server, {cors:config.cors);
 
     io.use(isSocketAuthenticated)
 
